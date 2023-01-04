9624b64f254fb623db6e5615c1f6c88ec42f8374
Anahita Sarafraz <anah0116@stud.kea.dk>
2023-01-04 20:47:09 +0100

    menu og animation på pil, 04.01 20:47

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ const menu = document.querySelector(".menu_liste");
 burger.addEventListener("click", burgermenu);
 
 function burgermenu() {
-  //   burger.classList.toggle("aktiv");
+  burger.classList.toggle("burger_aktiv");
   menu.classList.toggle("aktiv");
 }
 
@@ -14,4 +14,13 @@ document.querySelector(".nav_link").addEventListener("click", menu_link_klikket)
 function menu_link_klikket() {
   burger.classList.remove("aktiv");
   menu.classList.remove("aktiv");
+}
+
+// pil move
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector(".pil_forside").classList.add("pil_ani");
 }
